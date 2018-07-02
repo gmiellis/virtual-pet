fb127e034645370705e38010484097b3dc54caab
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-07-02 23:41:29 +0100

    refactored an formatted code

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,199 +1,195 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
-        expect(new Pet('Fido')).toBeInstanceOf(Object);
-    });
+  it('returns an object', () => {
+    expect(new Pet('Fido')).toBeInstanceOf(Object);
+  });
 
-    it('sets the name property', () => {
-        const pet = new Pet('Fido');
-        expect(pet.name).toEqual('Fido');
-    });
+  it('sets the name property', () => {
+    const pet = new Pet('Fido');
+    expect(pet.name).toEqual('Fido');
+  });
 
-    it('has a initial age of 0', () => {
-        const pet = new Pet('Fido');
-        expect(pet.age).toEqual(0);
-    });
+  it('has a initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
+  });
 
-    it('has an initial hunger value of 0', () => {
-        const pet = new Pet('Fido');
-        expect(pet.hunger).toEqual(0);
-    });
+  it('has an initial hunger value of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.hunger).toEqual(0);
+  });
 
-    it('has an initial fitness of 10', () => {
-        const pet = new Pet('Fido');
-        expect(pet.fitness).toEqual(10);
-    });
+  it('has an initial fitness of 10', () => {
+    const pet = new Pet('Fido');
+    expect(pet.fitness).toEqual(10);
+  });
 });
 
 describe('growUp', () => {
-    it('increments the age by 1', () => {
-        const pet = new Pet('Fido');
-        pet.growUp();
-        expect(pet.age).toEqual(1);
-    });
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  });
 
-    it('throws an error if the pet is not alive', () => {
+  it('throws an error if the pet is not alive', () => {
 
-        const pet = new Pet('Fido');
-        pet.age = 30;
+    const pet = new Pet('Fido');
+    pet.age = 30;
 
-        expect(pet.growUp).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
-    });
+    expect(pet.growUp).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
+  });
 });
 
 describe('hunger', () => {
 
-    it('increments hunger by 5 when growUp actioned', () => {
-        const pet = new Pet('Fido');
-        pet.growUp();
-        expect(pet.hunger).toEqual(5);
-    });
+  it('increments hunger by 5 when growUp actioned', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
 });
 
 describe('fitness', () => {
 
-    it('decreases the fitness by 3 when growUp actioned', () => {
-        const pet = new Pet('Fido');
-        pet.growUp();
-        expect(pet.fitness).toEqual(7);
-    });
+  it('decreases the fitness by 3 when growUp actioned', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  });
 });
 
 describe('walk', () => {
 
-    it('increases fitness by 4', () => {
+  it('increases fitness by 4', () => {
 
-        const pet = new Pet('Fido');
-        pet.fitness = 4
-        pet.walk();
-        expect(pet.fitness).toEqual(8);
-    });
+    const pet = new Pet('Fido');
+    pet.fitness = 4;
+    pet.walk();
+    expect(pet.fitness).toEqual(8);
+  });
 
-    it('increases fitness by to a max of 10', () => {
+  it('increases fitness by to a max of 10', () => {
 
-        const pet = new Pet('Fido');
-        pet.fitness = 8;
-        pet.walk();
+    const pet = new Pet('Fido');
+    pet.fitness = 8;
+    pet.walk();
 
-        expect(pet.fitness).toEqual(10);
-    });
+    expect(pet.fitness).toEqual(10);
+  });
 
-    it('throws an error if the pet is not alive', () => {
+  it('throws an error if the pet is not alive', () => {
 
-        const pet = new Pet('Fido');
-        pet.age = 30;
+    const pet = new Pet('Fido');
+    pet.age = 30;
 
-        expect(pet.walk).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
-    });
+    expect(pet.walk).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
+  });
 });
 
 describe('feed', () => {
 
-    it('decreases hunger by 3', () => {
+  it('decreases hunger by 3', () => {
 
-        const pet = new Pet('Fido');
-        pet.growUp();
-        pet.feed();
+    const pet = new Pet('Fido');
+    pet.growUp();
+    pet.feed();
 
-        expect(pet.hunger).toEqual(2);
-    });
+    expect(pet.hunger).toEqual(2);
+  });
 
-    it('throws an error if the pet is not alive', () => {
+  it('throws an error if the pet is not alive', () => {
 
-        const pet = new Pet('Fido');
-        pet.age = 30;
+    const pet = new Pet('Fido');
+    pet.age = 30;
 
-        expect(pet.feed).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
-    });
+    expect(pet.feed).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
+  });
 });
 
 describe('checkUp', () => {
 
-    it('alerts if fitness < 3', () => {
+  it('alerts if fitness < 3', () => {
 
-        const pet = new Pet('Fido');
-        pet.fitness = 3;
-        pet.checkUp();
-        expect(pet.checkUp()).toEqual('I need a walk');
-    });
+    const pet = new Pet('Fido');
+    pet.fitness = 3;
+    pet.checkUp();
+    expect(pet.checkUp()).toEqual('I need a walk');
+  });
 
-    it('alerts if hunger > 5', () => {
+  it('alerts if hunger > 5', () => {
 
-        const pet = new Pet('Fido');
-        pet.hunger = 5;
-        pet.checkUp();
-       expect(pet.checkUp()).toEqual('I am hungry'); 
-    });
+    const pet = new Pet('Fido');
+    pet.hunger = 5;
+    pet.checkUp();
+    expect(pet.checkUp()).toEqual('I am hungry'); 
+  });
 
-    it('alerts if fitness < 3 & hunger > 5', () => {
-        const pet = new Pet('Fido');
-        pet.fitness = 2;
-        pet.hunger = 6;        
-        pet.checkUp();
+  it('alerts if fitness < 3 & hunger > 5', () => {
+    const pet = new Pet('Fido');
+    pet.fitness = 2;
+    pet.hunger = 6;        
+    pet.checkUp();
 
-        expect(pet.checkUp()).toEqual('I am hungry AND I need a walk');
-    });
-    
-    it('alerts if fitness > 3 & hunger < 5', () => {
+    expect(pet.checkUp()).toEqual('I am hungry AND I need a walk');
+  });
 
-        const pet = new Pet('Fido');
-        pet.fitness = 6;
-        pet.hunger = 4;
-        pet.checkUp();
+  it('alerts if fitness > 3 & hunger < 5', () => {
 
-        expect(pet.checkUp()).toEqual('I feel great!');
-    });
+    const pet = new Pet('Fido');
+    pet.fitness = 6;
+    pet.hunger = 4;
+    pet.checkUp();
 
-    it('throws an error if the pet is not alive', () => {
+    expect(pet.checkUp()).toEqual('I feel great!');
+  });
 
-        const pet = new Pet('Fido');
-        pet.age = 30;
+  it('throws an error if the pet is not alive', () => {
 
-        expect(pet.checkUp).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
-    });
+    const pet = new Pet('Fido');
+    pet.age = 30;
+
+    expect(pet.checkUp).toThrow('Your pet is dead - so dead - deader than a dead thing on dead day :(');
+  });
 });
 
 describe('isAlive', () => {
 
-    it('alerts pet dead if age > 30', () => {
-        
-        const pet = new Pet('Fido');
-        pet.age = 31;
-        pet.isAlive;
+  it('alerts pet dead if age > 30', () => {
 
-        expect(pet.isAlive).toBe(false);
-    });
+    const pet = new Pet('Fido');
+    pet.age = 31;
 
-    it('alerts pet dead if fitness <= 0', () => {
+    expect(pet.isAlive).toBe(false);
+  });
 
-        const pet = new Pet('Fido');
-        pet.age = 25;
-        pet.fitness = -1;
-        pet.isAlive;
+  it('alerts pet dead if fitness <= 0', () => {
 
-        expect(pet.isAlive).toBe(false);
-    });
+    const pet = new Pet('Fido');
+    pet.age = 25;
+    pet.fitness = -1;
 
-    it('alerts pet dead if hunger >= 10', () => {
+    expect(pet.isAlive).toBe(false);
+  });
 
-        const pet = new Pet('Fido');
-        pet.age = 25;
-        pet.hunger = 12;
-        pet.isAlive;
+  it('alerts pet dead if hunger >= 10', () => {
 
-        expect(pet.isAlive).toBe(false);
-    });
+    const pet = new Pet('Fido');
+    pet.age = 25;
+    pet.hunger = 12;
 
-    it('alerts pet alive if all ok', () => {
+    expect(pet.isAlive).toBe(false);
+  });
 
-        const pet = new Pet('Fido');
-        pet.age = 25;
-        pet.hunger = 8;
-        pet.fitness = 5;
-        pet.isAlive;
+  it('alerts pet alive if all ok', () => {
 
-        expect(pet.isAlive).toBe(true);
-    });
+    const pet = new Pet('Fido');
+    pet.age = 25;
+    pet.hunger = 8;
+    pet.fitness = 5;
+
+    expect(pet.isAlive).toBe(true);
+  });
 
 });
--- src/pet.js
+++ src/pet.js
@@ -3,78 +3,76 @@ const MINIMUM_HUNGER = 0;
 
 function Pet(name) {
 
-        this.name = name;
-        this.age = 0;
-        this.hunger = 0;
-        this.fitness = 10;
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
 
 };
 
 Pet.prototype = {
 
-        get isAlive() {
+  get isAlive() {
 
-                return this.age < 30 && this.hunger < 10 && this.fitness > 0;
-        }
+    return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+  },
 };
 
-Pet.prototype.growUp = function() {
+Pet.prototype.growUp = function () {
 
-        if(!this.isAlive) {
-                throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
+  if (!this.isAlive) {
+    throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
 
-        } else {
-        this.age += 1;
-        this.hunger += 5;
-        this.fitness -= 3;
-        }
+  } else {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+  }
 };
 
-Pet.prototype.walk = function() {
+Pet.prototype.walk = function () {
 
-        if(!this.isAlive) {
-                throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
+  if (!this.isAlive) {
+    throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
         
-        } else if((this.fitness + 4) <= MAXIMUM_FITNESS) {
-                this.fitness += 4;
+  } else if((this.fitness + 4) <= MAXIMUM_FITNESS) {
+    this.fitness += 4;
 
-        } else {
-                this.fitness = MAXIMUM_FITNESS;
-
-        }
+  } else {
+    this.fitness = MAXIMUM_FITNESS;
+  }
 };
 
-Pet.prototype.feed = function() {
+Pet.prototype.feed = function () {
 
-        if(!this.isAlive) {
-                throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
+  if (!this.isAlive) {
+    throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
 
-        } else if((this.hunger - 3) >= MINIMUM_HUNGER) {
-                this.hunger -= 3;
-                
-        } else {
+  } else if ((this.hunger - 3) >= MINIMUM_HUNGER) {
+    this.hunger -= 3;
 
-                this.hunger = MINIMUM_HUNGER;
-        }
+  } else {
+    this.hunger = MINIMUM_HUNGER;
+  }
 };
 
-Pet.prototype.checkUp = function() {
+Pet.prototype.checkUp = function () {
 
-        if(!this.isAlive) {
-                throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
-        
-        } else if(this.fitness <= 3 && this.hunger >= 5) {
-                return 'I am hungry AND I need a walk';
+  if (!this.isAlive) {
+    throw new Error('Your pet is dead - so dead - deader than a dead thing on dead day :(');
 
-        } else if (this.hunger >= 5) {
-                return 'I am hungry';
+  } else if (this.fitness <= 3 && this.hunger >= 5) {
+    return 'I am hungry AND I need a walk';
 
-        } else if (this.fitness <= 3) {
-                return 'I need a walk';
+  } else if (this.hunger >= 5) {
+    return 'I am hungry';
 
-        } else {
-                return 'I feel great!';
-        }
+  } else if (this.fitness <= 3) {
+    return 'I need a walk';
+
+  } else {
+    return 'I feel great!';
+  }
 };
 
 module.exports = Pet;
